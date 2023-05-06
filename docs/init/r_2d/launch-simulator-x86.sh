e314c3be9075cf4a7c4526330192c31d0bf21a45
setrar <setrar@valiha.com>
2023-05-05 22:46:56 -0400

    .

---
+++ docs/init/r_2d/launch-simulator-x86.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+APP_NAME="$(cat Cargo.toml | dasel -r toml '.package.name')"
+BUNDLE_ID="$(cat Cargo.toml | dasel -r toml '.package.metadata.bundle.identifier')"
+
+cargo bundle --target x86_64-apple-ios
+xcrun simctl boot "iPhone 14 Pro"  
+open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app 
+xcrun simctl install booted "target/x86_64-apple-ios/debug/bundle/ios/$APP_NAME.app"
+xcrun simctl launch --console booted "$BUNDLE_ID"
+
+
